708e70a3475a5ba30c6edab2057d13a280974f28
Ata <budakata2001@gmail.com>
2024-04-04 01:06:25 +0300

    Created login page

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
