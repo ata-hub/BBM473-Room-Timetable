2f53e5c8dd995036f2cc74e8f1f1c56421213a50
Ata <budakata2001@gmail.com>
2024-05-30 00:39:27 +0300

    connected feature request and student request
    Backend connection is made, however, get_user_rooms must return id as well. For now, we are getting error because service requires frontend to send id, but it can only send name because it doesnt have the id

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import datetime
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from service import MyException, RoomService, UserService
+
 app = Flask(__name__)
 
 users = {
@@ -38,9 +40,9 @@ def login():
 
 # Dummy room data (replace with actual data from the database)
 room_data = [
-    {"name": "Room 1"},
-    {"name": "Room 2"},
-    {"name": "Room 3"}
+    "Room 1",
+    "Room 2",
+     "Room 3"
 ]
 
 @app.route('/student')
@@ -48,6 +50,7 @@ def studentPage():
     # Define time slots
     time_slots = [f"{hour}:00" for hour in range(8, 20)]
     #TODO roomları backendden al (permissionlardan al)
+    room_data = UserService().get_user_rooms()
     return render_template('student.html', 
                            room_data=room_data, 
                            time_slots=time_slots, 
@@ -58,6 +61,8 @@ def instructorPage():
     # Define time slots
     time_slots = [f"{hour}:00" for hour in range(8, 20)]
     #TODO roomları backendden al (bütün departman odaları)
+    room_data = UserService().get_user_rooms()
+    print("rooms for instructor:",room_data)
     return render_template('instructor.html', 
                            room_data=room_data, 
                            time_slots=time_slots, 
@@ -68,39 +73,76 @@ def adminPage():
     # Define time slots
     time_slots = [f"{hour}:00" for hour in range(8, 20)]
     #TODO roomları backendden al sistmdeki tüm odalar
+    room_data = UserService().get_user_rooms()
     return render_template('admin.html',
                             room_data=room_data,
                             time_slots=time_slots,
                             user_role="admin")
 
-
+#instructor making student request
 @app.route('/student_request', methods=['POST'])
 def student_request():
     student_username = request.form['studentUsername']
     student_room = request.form['studentRoom']
     # TODO add student request to db (permissions table and student_request_permission)
-    # Process the request here
-    # TODO db işlemleri olumlu gerçekleştiyse return jsonify(success=True)
-    # TODO db işlemleri olumsuz gerçekleştiyse return jsonify(success=False)
-    # TODO db üstteki jsonifyları implement ettikten sonra aşağıdaki satırları sil
-    # For now, we'll simulate success or failure
-    if student_username and student_room:
-        return jsonify(success=True)
-    return jsonify(success=False)
+    requestDto = {
+        'username': student_username,
+        'room': student_room
+    }
+    try:
+        # Call the request_permission method with the requestDto
+        result = UserService().request_permission(requestDto)
+        if(result == 'True'):
+            return jsonify(success=True)
+    except MyException as e:
+        # Handle custom exceptions
+        return jsonify(success=False, message=str(e))
+    except Exception as e:
+        # Handle other exceptions
+        return jsonify(success=False, message="An error occurred while processing the request.")
+    
+@app.route('/list_features', methods=['GET'])
+def list_features():
+    features = RoomService().list_features()
+    return jsonify(features=features)
+
 
 @app.route('/feature_request', methods=['POST'])
 def feature_request():
-    feature = request.form['feature']
-    feature_room = request.form['featureRoom']
-    # TODO add feature request to db (istenen feature feature tableda yoksa ekle and feature_request_permission)
-    # Process the request here
-    # TODO db işlemleri olumlu gerçekleştiyse return jsonify(success=True)
-    # TODO db işlemleri olumsuz gerçekleştiyse return jsonify(success=False)
-    # TODO db üstteki jsonifyları implement ettikten sonra aşağıdaki satırları sil
-    # For now, we'll simulate success or failure
-    if feature and feature_room:
-        return jsonify(success=True)
-    return jsonify(success=False)
+    feature = request.form.get('existingFeatures')
+    print("feature is:",feature)
+    new_feature = request.form.get('feature')
+    feature_room = request.form.get('room')
+    description = request.form.get('description')
+
+    if feature == 'other':
+        feature_id = None
+    else:
+        feature_id = feature
+    print("feature_id is:", feature_id)
+    requestDto = {
+        'feature_id': feature_id,
+        'new_feature': new_feature if feature_id is None else None,
+        'room': feature_room,
+        'description': description
+    }
+    print("dto is:",requestDto)
+    #feature dropdownunda seçilen option other ise requestDto da feature_id null olacak, new_feature değeri girilen string olacak
+    #eğer dropdownda other dışında bir şey seçilirse feature_id o seçilen featureın idsi olacak
+    try:
+        # Call the request_permission method with the requestDto
+        result = RoomService().request_feature(requestDto)
+        if(result == 'True'):
+            return jsonify(success=True)
+    except MyException as e:
+        # Handle custom exceptions
+        print("MyException Error occured:",str(e) )
+        return jsonify(success=False, message=str(e))
+        
+    except Exception as e:
+        # Handle other exceptions
+        print("Exception occured:",str(e) )
+        return jsonify(success=False, message="An error occurred while processing the request.")
 
 @app.route('/admin/pending_student_request')
 def pending_student_requests():
